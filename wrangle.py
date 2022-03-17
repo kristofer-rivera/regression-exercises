48ef2faa0ce09f0a517bdf7ff3ff646cff11d894
Kristofer Rivera <kristofer.e.rivera@gmail.com>
2022-03-17 15:59:42 -0500

    Adjustments and additions made after review.

--- wrangle.py
+++ wrangle.py
@@ -1,7 +1,12 @@
 from env import host, user, password, get_db_url
 import pandas as pd 
 import os
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 
 
 
@@ -20,27 +25,138 @@ def get_zillow_raw(use_cache=True):
     else:
         print('Retrieving data from mySQL server...')
         df = pd.read_sql('''
-        SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017
-        JOIN propertylandusetype
+        SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips FROM properties_2017 
+        LEFT JOIN propertylandusetype 
         USING (propertylandusetypeid)
-        WHERE propertylandusetypeid = 261;''' , get_db_url('zillow'))
+        WHERE propertylandusedesc IN ('Single Family Residential', 'Inferred Single Family Residential');''' , get_db_url('zillow'))
         print('Caching data as csv file for future use...')
         df.to_csv(filename, index=False)
         return df
 
-def wrangle_zillow():
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
     '''
-    This function takes in no arguments, uses the get_zillow_raw function to establish a connection with the mysql database and acquire a raw version 
-    of the zillow dataframe. The function then performes the necesary operations to rename columns and drop all null values returning a cleaned zillow dataframe with zero nulls.
-    '''
-    #get zillow data
-    df = get_zillow_raw()
-    #drop all nulls
-    df = df.dropna()
-    #convert floats to int
-    df = df.astype('int')
-    #renaming columns for clarity
+    
+    for col in col_list:
+
+        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
+        
+    return df
+
+
+def get_hist(df):
+    ''' Gets histographs of acquired continuous variables'''
+    
+    plt.figure(figsize=(16, 3))
+
+    # List of columns
+    cols = [col for col in df.columns if col not in ['fips', 'year_built']]
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot nos should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display histogram for column.
+        df[col].hist(bins=5)
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # turn off scientific notation
+        plt.ticklabel_format(useOffset=False)
+
+        plt.tight_layout()
+
+    plt.show()
+
+
+
+def get_box(df):
+    ''' Gets boxplots of acquired continuous variables'''
+    
+    # List of columns
+    cols = ['bedrooms', 'bathrooms', 'sqft', 'tax_value', 'tax_amount']
+
+    plt.figure(figsize=(16, 3))
+
+    for i, col in enumerate(cols):
+
+        # i starts at 0, but plot should start at 1
+        plot_number = i + 1 
+
+        # Create subplot.
+        plt.subplot(1, len(cols), plot_number)
+
+        # Title with column name.
+        plt.title(col)
+
+        # Display boxplot for column.
+        sns.boxplot(data=df[[col]])
+
+        # Hide gridlines.
+        plt.grid(False)
+
+        # sets proper spacing between plots
+        plt.tight_layout()
+
+    plt.show()
+
+
+def prepare_zillow(df):
+    ''' Prepare zillow data for exploration'''
+
+    #rename columns for clarity
     df = df.rename(columns = {'bedroomcnt': 'bedrooms', 'bathroomcnt': 'bathrooms', 
                                   'calculatedfinishedsquarefeet': 'sqft', 'taxvaluedollarcnt': 'tax_value', 
                                   'yearbuilt': 'year_built', 'taxamount': 'tax_amount'})
-    return df
+
+    # removing outliers
+    df = remove_outliers(df, 1.5, ['bedrooms', 'bathrooms', 'sqft', 'tax_value', 'tax_amount'])
+    
+    # get distributions of numeric data
+    get_hist(df)
+    get_box(df)
+
+    #Imputing for null values in year_built
+    imputer = SimpleImputer(strategy='median') #build imputer
+    imputer.fit(df[['year_built']]) #fit to data
+    df[['year_built']] = imputer.transform(df[['year_built']])
+    
+    # I will first cast year built and fips as intergers to remove the zeros, then cast as objects
+    df.year_built = df.year_built.astype(int)
+    df.fips = df.fips.astype(int)
+    
+    df.year_built = df.year_built.astype(object)
+    df.fips = df.fips.astype(object)
+    
+    # train/validate/test split
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+    
+    return train, validate, test  
+        
+
+
+def wrangle_zillow():
+    '''Acquire and prepare data from Zillow database for explore'''
+    train, validate, test = prepare_zillow(get_zillow_raw())
+    
+    return train, validate, test
